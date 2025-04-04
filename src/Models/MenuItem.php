4cf5a0734912464d4c478bf6622549fb81539fdc
devgobalo <118296125+devgobalo@users.noreply.github.com>
2025-04-04 13:55:06 +0200

    Update MenuItem.php

--- src/Models/MenuItem.php
+++ src/Models/MenuItem.php
@@ -41,7 +41,8 @@ class MenuItem extends Model
     public function children()
     {
         return $this->hasMany(Voyager::modelClass('MenuItem'), 'parent_id')
-            ->with('children');
+            ->with('children')
+            ->orderBy('order');
     }
 
     public function menu()
@@ -90,7 +91,7 @@ class MenuItem extends Model
 
     public function getParametersAttribute()
     {
-        return json_decode($this->attributes['parameters'] ?? '');
+        return json_decode($this->attributes['parameters']);
     }
 
     public function setParametersAttribute($value)
